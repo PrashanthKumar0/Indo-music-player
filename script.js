33d4447cbd11e6e1141af2fb39f15988019242b4
PrashanthKumar0 <prashanthgiri786@gmail.com>
2021-04-09 08:53:58 -0700

    new feature of audio visualizer

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 onload=init;
-
+var baseUrl="./";
 function $(el){
     return document.getElementById(el);
 }
@@ -15,32 +15,44 @@ const PlayList=[
     {
         name:"Ignite",
         artist:"Alan Walker",
-        src:"./assets/music/K-391_Alan_Walker_-_Ignite_feat._Julie_Bergan_Seungri.mp3",
-        cover:"./assets/cover/alan-walker.jpg"
+        src:baseUrl+"assets/music/K-391_Alan_Walker_-_Ignite_feat._Julie_Bergan_Seungri.mp3",
+        cover:baseUrl+"assets/cover/alan-walker.jpg"
     },
     {
         name:"Melody",
         artist:"Cadium",
-        src:"./assets/music/Cadmium - Melody (feat. Jon Becker) .mp3",
-        cover:"./assets/cover/cadium-melody.jpeg"
+        src:baseUrl+"assets/music/Cadmium - Melody (feat. Jon Becker) .mp3",
+        cover:baseUrl+"assets/cover/cadium-melody.jpeg"
+    },
+    {
+        name:"Seasons",
+        artist:"Cadium (feat. Harley Bird)",
+        src:baseUrl+"assets/music/Seasons  [NCS Release].mp3",
+        cover:baseUrl+"assets/cover/Seasons albumArt.jpg"
     },
     {
         name:"Different Way",
         artist:"Dj Snake",
-        src:"./assets/music/DJ Snake Lauv - A Different Way.mp3",
-        cover:"./assets/cover/different-way.jpg"
+        src:baseUrl+"assets/music/DJ Snake Lauv - A Different Way.mp3",
+        cover:baseUrl+"assets/cover/different-way.jpg"
     },
     {
         name:"On The Floor",
         artist:"Jennifer Lopez",
-        src:"./assets/music/Jennifer Lopez - On the floor feat. Pitbul .mp3",
-        cover:"./assets/cover/on the floor.jpg"
+        src:baseUrl+"assets/music/Jennifer Lopez - On the floor feat. Pitbul .mp3",
+        cover:baseUrl+"assets/cover/on the floor.jpg"
     },
     {
         name:"Vale Vale",
         artist:"Alok Zafrir",
-        src:"./assets/music/Vale Vale Ho .mp3",
-        cover:"./assets/cover/vale vale.png"
+        src:baseUrl+"assets/music/Vale Vale Ho .mp3",
+        cover:baseUrl+"assets/cover/vale vale.png"
+    },
+    {
+        name:"Zombie",
+        artist:"Bad Wolves",
+        src:baseUrl+"assets/music/Bad Wolves - Zombie.mp3",
+        cover:baseUrl+"assets/cover/wolf.jpg"
     },
     
 ];
@@ -48,9 +60,18 @@ const PlayList=[
 const audio=new Audio();
 audio.src=PlayList[0].src;
 var currentElm;
+var currentCover=new Image(); //will be used by visualizer
 function addSongToPlayList(obj){
     PlayList.push(obj);
     ShowSongs(obj);
+}
+
+function ShowSongs() {
+    $("playList").innerHTML='';
+    for(var i=0;i<PlayList.length;i++){
+        InsertSong(PlayList[i]);
+    }
+    $("player").scrollIntoView(0,$("player").offsetHeight)
 }
 
 audio.ontimeupdate=function(){
@@ -97,7 +118,8 @@ function playSong(e,el=0){
     _("#mainCover>img").src=cover;
     $("infoName").innerHTML=name;
     $("infoAuthor").innerHTML=artist;
-
+    $("titleVisual").innerHTML=name;
+    currentCover.src=cover;
     var _lst=_(".list",1);
     var _btn=_(".play",1);
     for(var i=0;i<_lst.length;i++){
@@ -177,21 +199,24 @@ function init(){
     $("playNowBtn").onclick=togglePlay; 
     $("playPreviousBtn").onclick=playPrevious;
     $("playNextBtn").onclick=playNext;
+    $("showVisual").onclick=showVisuals;
+    $("backToPlayerV").onclick=backToPlayerV;
+    $("file").onchange=hFileUpload;
+    ShowSongs();
     addSeekEvents();
     
-    for(var i=0;i<PlayList.length;i++){
-        InsertSong(PlayList[i]);
-    }
 }
 
 audio.onpause=function(){
     $("playNowBtn").innerHTML='|>';
     currentElm.innerHTML='|>';
 }
+
 audio.onplay=function(){
     $("playNowBtn").innerHTML='||';
     currentElm.innerHTML='||';
 }
+
 function togglePlay(){
     if(audio.paused){
         audio.play();
@@ -199,6 +224,44 @@ function togglePlay(){
         audio.pause();
     }
 }
+
+function hFileUpload(e){
+    e.preventDefault();
+    for (var a=0;a<$("file").files.length;a++){
+        var file=$("file").files[a];
+        url = file.urn || file.name;
+        ID3.loadTags(url, function() {
+            fileUploaded(url,URL.createObjectURL(file));
+          }, {
+            tags: ["title","artist","picture"],
+            dataReader: ID3.FileAPIReader(file)
+          });    
+    }
+}
+
+function fileUploaded(url,mp3Url){
+    var tags = ID3.getAllTags(url);
+    var image = tags.picture;
+    if (image) {
+      var base64String = "";
+      for (var i = 0; i < image.data.length; i++) {
+          base64String += String.fromCharCode(image.data[i]);
+      }
+      var base64 = "data:" + image.format + ";base64," +
+              window.btoa(base64String);
+      image=(base64);
+    } else {
+        image=baseUrl+"assets/cover/unknown.jpg";
+    }
+
+    addSongToPlayList({
+        name:((tags.title || "(Unknown)").split('.')[0]).replace(/[0-9]/gm,"").replace(/[^a-z]/gmi," "),
+        artist:tags.artist ||"(Unknown)",
+        src:mp3Url,
+        cover:image
+    });
+}
+
 
 function addSeekEvents(){
     
@@ -219,3 +282,116 @@ function addSeekEvents(){
         audio.currentTime=(((e.clientX - $("seekTrackThumb").offsetWidth/2   )/$("seekBar").offsetWidth)*audio.duration);
     };
 }
+
+//visualization part
+
+var ctx,cnvs,aCtx,analyser,mediaStream,W,H,visualArray;
+var isInitialized=0;
+var shouldAnimate=0;
+var then;
+
+var coverBaseW=150;
+var bounce=10;
+var theta=0;
+
+function backToPlayerV(){
+    $("player").style.filter="grayscale(0)";
+    $("player").style.opacity="1";
+    setTimeout(function(){
+        $("player").style.display="flex";
+    },350)
+    $("visuals").classList.remove("show");    
+
+    shouldAnimate=0;
+}
+
+function showVisuals(){
+    $("player").style.filter="grayscale(0.6)";
+    $("player").style.opacity="0";
+    setTimeout(function(){
+        $("player").style.display="none";
+    },350)
+    $("visuals").classList.add("show"); 
+    
+    //initialize all variables
+    if(!isInitialized){
+        isInitialized=1;
+        cnvs=$("cnvs");
+        W=cnvs.width=innerWidth-cnvs.offsetLeft;
+        H=cnvs.height=innerHeight-cnvs.offsetTop;
+        ctx=cnvs.getContext("2d");
+        aCtx=new AudioContext();
+        analyser=aCtx.createAnalyser();
+        mediaStream=aCtx.createMediaElementSource(audio);
+        shouldAnimate=1;
+        visualArray=new Uint8Array(360+400);
+    } else {
+        
+        W=cnvs.width=innerWidth;
+        H=cnvs.height=innerHeight;
+        shouldAnimate=1;
+    }
+    mediaStream.connect(analyser);
+    analyser.connect(aCtx.destination);
+    then=performance.now();
+    animateVisuals();
+}
+
+function animateVisuals(){
+    if(!shouldAnimate) return;
+    var _pixelX=(W/2)+10*Math.cos(theta);
+    var _pixeY=(H/2)+10*Math.sin(theta);
+    var pxl=ctx.getImageData(_pixelX,_pixeY,1,1).data;
+    ctx.fillStyle="rgba(38, 40, 43,0.2)";
+    ctx.fillRect(0,0,W,H);
+    theta+=0.01;
+    bounce=(visualArray[100]/10);
+    analyser.getByteFrequencyData(visualArray);
+    var ratio=(currentCover.height/currentCover.width);
+    
+    ctx.fillStyle="rgba("+(pxl[0]+100)+","+(pxl[1]+100)+","+(100+pxl[2])+",1)";
+    ctx.save();
+    ctx.translate(W/2,H/2);
+    ctx.rotate(-theta);
+    ctx.beginPath();
+    var incVal=Math.floor(1+((performance.now()-then)/20));
+    for(var a=0;a<360;a+=incVal){
+        var _angle=(Math.PI/180)*a;
+        var _r=(coverBaseW+bounce+5+visualArray[a]);
+        // if(_r >= (W/2 - coverBaseW) ){
+        //     _r=_r-(W/2 - coverBaseW); //constrain
+        // }
+        var _x=(_r*Math.cos(_angle))/2;
+        var _y=(_r*Math.sin(_angle))/2;
+        if(a==0){
+            ctx.moveTo(_x,_y);
+        } else {
+            ctx.lineTo(_x,_y);
+        }
+    }
+    ctx.fill();
+    // ctx.stroke();
+    ctx.restore();
+    
+    
+    
+    ctx.fillStyle="rgba("+(pxl[0])+","+(pxl[1])+","+(pxl[2])+",1)";
+    //cover
+    ctx.save();
+    ctx.translate(W/2,H/2);
+    ctx.rotate(theta);
+        ctx.beginPath();
+        ctx.arc(0,0,((coverBaseW+bounce)/2)+5,0,Math.PI*2);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.arc(0,0,((coverBaseW+bounce)/2),0,Math.PI*2);
+        ctx.clip();
+        ctx.drawImage(currentCover,-(coverBaseW+bounce)/2,(-(coverBaseW+bounce)/2),coverBaseW+bounce,ratio*(coverBaseW+bounce));    
+    ctx.restore();
+
+    
+    then=performance.now();
+    requestAnimationFrame(animateVisuals);
+}
+
+
